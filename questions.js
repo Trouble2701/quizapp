2bbec6c3206f598c8ee67f6dfc679376eb7a5d76
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2023-12-18 21:25:51 +0100

    Questions in JSON Array

---
+++ questions.js
@@ -0,0 +1,18 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": "3"
+    },
+    {
+        "question": "Wie bindet man eine Website in eine Website ein?",
+        "answer_1": "iframe",
+        "answer_2": "Templete",
+        "answer_3": "Array",
+        "answer_4": "Javascript",
+        "right_answer": "1"
+    }
+];
