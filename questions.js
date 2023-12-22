5dc00f421ab13ddff52c088b12736b2b2cbe0c5a
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2023-12-22 14:04:48 +0100

    Questions Added

--- questions.js
+++ questions.js
@@ -1,18 +1,82 @@
 let questions = [
     {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right_answer": "3"
+        "question": "Was ist ein Ausgabegerät?",
+        "answer_1": "Bildschirm",
+        "answer_2": "Webcam",
+        "answer_3": "Lautsprecher",
+        "answer_4": "Maus",
+        "right_answer": 1
     },
     {
-        "question": "Wie bindet man eine Website in eine Website ein?",
-        "answer_1": "iframe",
-        "answer_2": "Templete",
-        "answer_3": "Array",
-        "answer_4": "Javascript",
-        "right_answer": "1"
+        "question": "Was ist Kein Eingabegerät?",
+        "answer_1": "Touchpad",
+        "answer_2": "Tastatur",
+        "answer_3": "Beamer",
+        "answer_4": "Maus",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist richtig?",
+        "answer_1": "Ein Netzteil ist ein anderer Begriff für Modem",
+        "answer_2": "Ein Netzteil dient zur Stromversorgung des Computers",
+        "answer_3": "Ein Netzteil schützt den Computer vor Vieren",
+        "answer_4": "Ein Netzteil ist dazu da, um den Computer mit einem Netzwerk z.B. einem LAN (Local Area Network) verbinden zu können.",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist falsch?",
+        "answer_1": "Der Prozessor wird auch CPU genannt und die Abkürzung steht für Central Processing Unit (zentrale Recheneinheit bzw. zentrale Verarbeitungseinheit)",
+        "answer_2": "Ohne Prozessor ist der Computer nicht funktionstüchtig",
+        "answer_3": "Die Prozessortgeschwindigkeit wird in Byte angegeben (z.B. 3,2GB)",
+        "answer_4": "Die Prozessortgeschwindigkeit wird in Hertz angegeben (z.B. 3,2GHz)",
+        "right_answer": 3
+    },
+    {
+        "question": "Wählen Sie die Antwort, wo die Speichereinheiten der Größe nach angeordnet sind",
+        "answer_1": "Byte, Bit, Kilobyte, Megabyte, Gigabyte, Terabyte, Petabyte",
+        "answer_2": "Bit, Byte, Kilobyte, Gigabyte, Terabyte, Petabyte, Megabyte",
+        "answer_3": "Bit, Byte, Kilobyte, Megabyte, Terabyte, Gigabyte, Petabyte",
+        "answer_4": "Bit, Byte, Kilobyte, Megabyte, Gigabyte, Terabyte, Petabyte",
+        "right_answer": 4
+    },
+    {
+        "question": "Was ist richtig?",
+        "answer_1": "Ein Zeichen benötigt ein Megabyte Speicherplatz",
+        "answer_2": "Ein Zeichen benötigt ein Kilobyte Speicherplatz",
+        "answer_3": "Ein Zeichen benötigt ein Byte Speicherplatz",
+        "answer_4": "Ein Zeichen benötigt ein Bit Speicherplatz",
+        "right_answer": 4
+    },
+    {
+        "question": "Was ist ein anderer Begriff für Prozessorgeschwindigkeit?",
+        "answer_1": "Kilowattstunden",
+        "answer_2": "Taktfrequenz",
+        "answer_3": "Umdrehungszahl",
+        "answer_4": "Stromfluss",
+        "right_answer": 2
+    },
+    {
+        "question": "In welcher Maßeinheit wird die Taktfrequenz angegeben?",
+        "answer_1": "kg",
+        "answer_2": "GHz",
+        "answer_3": "Joule",
+        "answer_4": "km/h",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist kein Zeigegrät?",
+        "answer_1": "Touchpad",
+        "answer_2": "Maus",
+        "answer_3": "Trackball",
+        "answer_4": "Scanner",
+        "right_answer": 3
+    },
+    {
+        "question": "Welches Gerät ist ein Eingabe- und Ausgabegerät zugleich?",
+        "answer_1": "Mikrofon",
+        "answer_2": "Beamer",
+        "answer_3": "Webcam",
+        "answer_4": "Touchscreen",
+        "right_answer": 4
     }
 ];
